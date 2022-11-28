ac4ae6d698b54a0117d7609e44d0acdb57c11068
happy-farah <far20180723@std.psut.edu.jo>
2022-11-28 11:54:27 +0300

    new AO

--- Android/AdminOwnerHatrick/app/src/main/java/com/example/adminownerhatrick/createField.kt
+++ Android/AdminOwnerHatrick/app/src/main/java/com/example/adminownerhatrick/createField.kt
@@ -4,7 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+<<<<<<< HEAD
 import android.widget.Button
+=======
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+>>>>>>> b1569ce (new AO)
 import com.google.firebase.ktx.Firebase
 
 class createField : AppCompatActivity() {
@@ -13,6 +19,7 @@ class createField : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_field)
+<<<<<<< HEAD
         storageRef = Firebase.s.reference;
         val btnSelectImage = findViewById<Button>(R.id.BTN)
         // button Click listener
@@ -26,5 +33,27 @@ class createField : AppCompatActivity() {
             // ActivityResultLauncher callback
             imagePickerActivityResult.launch(galleryIntent)
         }
+=======
+        val sportType: Spinner = findViewById(R.id.sportType)
+        var sports = arrayOf("Football", "Basketball", "Tennis", "Handball")
+        sportType.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sports)
+        val locat: Spinner = findViewById(R.id.fieldLocationSpn)
+        var locations = arrayOf("Jbiha", "Shafa Badran", "Abo Nsair", "Khalda")
+        locat.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, locations)
+
+//        storageRef = Firebase.s.reference;
+//        val btnSelectImage = findViewById<Button>(R.id.BTN)
+//        // button Click listener
+//// invoke on user interaction
+//        btnSelectImage.setOnClickListener {
+//            // PICK INTENT picks item from data
+//            // and returned selected item
+//            val galleryIntent = Intent(Intent.ACTION_PICK)
+//            // here item is type of image
+//            galleryIntent.type = "image/*"
+//            // ActivityResultLauncher callback
+//            imagePickerActivityResult.launch(galleryIntent)
+//        }
+>>>>>>> b1569ce (new AO)
     }
 }
